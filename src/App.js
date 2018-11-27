6b3df236576cdf048058715c6905723bbaa9e42f
Serhii <serhii@gmail.com>
2018-11-27 14:22:52 +0200

    do-it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Layout from './hoc/Layout/Layout'
+import Layout from './hoc/Layout/Layout';
+import MainPage from './containers/mainPage/mainPage';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import AboutAuthor from './containers/aboutAuthor/aboutAuthor'
+import Authorization from './containers/Authorization/Authorization'
 
 class App extends Component {
+
+    checkLogg(isLogg){
+        this.setState({
+            isLoggIn: isLogg
+        });
+    }
+
   render() {
     return (
-        <Layout>
-          
-        </Layout>
+        <Layout changeLink={this.checkLogg.bind(this)} >
+             <Switch>
+                 <Route path="/" exact component={Authorization} />
+                 <Route path="/main_page" component={MainPage}  />
+                 <Route path="/about_author" component={AboutAuthor} />
+                 <Redirect to={'/'} />
+            </Switch>
+         </Layout>
+
     );
   }
 }
